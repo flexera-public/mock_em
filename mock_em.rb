d733a14047afa4f043fea2b78c5985f389893df3
jim-slattery-rs <jim.slattery@rightscale.com>
2013-11-13 10:24:22 -0500

    acu108496 - add_periodic_timer and cancel_timer both seem to work!

--- mock_em.rb
+++ mock_em.rb
@@ -11,6 +11,7 @@ module CloudGatewaySupport
 
       @next_tick_procs = []
       @scheduled_tasks = ScheduledTasks.new(@logger)
+      @timer_objects = []
       @is_stopped = false
       @clock_millis = 0
     end
@@ -23,9 +24,9 @@ module CloudGatewaySupport
       @logger.info("Beginning tick loop.")
       @tick_count = 0
       while (!@is_stopped)
-        # prepare this tick
         @tick_count += 1
         @clock_millis += TICK_MILLIS_STEP
+        @logger.info "Preparing tick ##{@tick_count}, clock=#{@clock_millis}"
 
         this_tick_procs = @scheduled_tasks.pop_due_tasks(@clock_millis) + @next_tick_procs
         @next_tick_procs = []
@@ -37,7 +38,7 @@ module CloudGatewaySupport
             @logger.info "Nothing left to do! Returning."
             break
           else
-            @logger.info "Accelerating clock to: #{next_time}"
+            @logger.info "Nothing in this tick. Accelerating clock to: #{next_time}"
             @clock_millis = next_time
             this_tick_procs = @scheduled_tasks.pop_due_tasks(@clock_millis)
           end
@@ -64,17 +65,62 @@ module CloudGatewaySupport
       @next_tick_procs << block
     end
 
-    def add_timer(time_seconds, &block)
-      @logger.info "Adding timer task: time_seconds=#{time_seconds}"
-      @scheduled_tasks.add_task(@clock_millis + (time_seconds * 1000), &block)
+    def add_timer(time_seconds, reuse_timer=nil, &block)
+      timer = reuse_timer || MockTimer.new
+      @logger.info "Adding timer task: id=#{timer.id}, time_seconds=#{time_seconds}"
+      @scheduled_tasks.add_task(@clock_millis + (time_seconds * 1000)) do
+        if timer.is_cancelled
+          @logger.info "Skipping this timer task, it's already cancelled"
+        else
+          block.call
+        end
+      end
+      @timer_objects << timer
+      timer
     end
 
-    def add_periodic_timer(period, &block)
+    def add_periodic_timer(period_seconds, &block)
+      timer = MockTimer.new
+      @logger.info "Creating periodic timer task: id=#{timer.id}, period_seconds=#{period_seconds}"
+
+      recursive_block = nil
+      recursive_block = lambda do
+        if timer.is_cancelled
+          @logger.info "Skipping timer task id=#{timer.id}, it's already cancelled"
+        else
+          block.call
+        end
+        if !timer.is_cancelled
+          @logger.info "Rescheduling next run of periodic timer id=#{timer.id}"
+          add_timer(period_seconds, timer, &recursive_block)
+        end
+      end
+
+      add_timer(period_seconds, timer, &recursive_block)
     end
 
     def cancel_timer(timer)
+      #TODO: support looking up by timer ID as well
+      @timer_objects.delete(timer)
+      timer.cancel
     end
 
+
+    class MockTimer
+
+      @@id_seq = 0
+
+      attr_reader :id, :is_cancelled
+
+      def initialize
+        @is_cancelled = false
+        @id = @@id_seq += 1
+      end
+
+      def cancel
+        @is_cancelled = true
+      end
+    end
 
     class ScheduledTasks
       ScheduledTask = Struct.new(:timestamp, :proc)
@@ -90,7 +136,8 @@ module CloudGatewaySupport
       end
 
       def pop_due_tasks(timestamp)
-        due_tasks = @tasks.delete_if {|t| t.timestamp <= timestamp }
+        due_tasks = @tasks.take_while {|t| t.timestamp <= timestamp }
+        @tasks = @tasks - due_tasks
         @logger.info("Popped #{due_tasks.count} due tasks. Clock=#{timestamp}, due_times=#{due_tasks.map(&:timestamp).inspect}")
         due_tasks.map(&:proc)
       end
