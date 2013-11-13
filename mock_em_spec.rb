73a1d7d54c5d6aa30ff0a8ba88f7b9e92628a072
jim-slattery-rs <jim.slattery@rightscale.com>
2013-11-13 10:24:22 -0500

    acu108496 - add_periodic_timer and cancel_timer both seem to work!

--- mock_em_spec.rb
+++ mock_em_spec.rb
@@ -34,15 +34,65 @@ describe CloudGatewaySupport::MockEM do
       end
 
       it "should support add_timer" do
-        after_stop = false
+        counter = 0
         @em.run do
-          @em.add_timer(2) do
-            @em.stop
-            after_stop = true
+          @em.add_timer(1) do
+            counter.should == 0
+            counter += 1
+            @em.add_timer(1) do
+              counter.should == 1
+              @em.stop
+              counter +=1
+            end
           end
         end
-        after_stop.should == true
+        counter.should == 2
       end
+
+      it "should_support cancel_timer" do
+        @em.run do
+          timer = @em.add_timer(1) do
+            fail
+          end
+
+          @em.cancel_timer(timer)
+
+          @em.add_timer(2) do
+            @em.stop
+          end
+        end
+      end
+
+      it "should support add_periodic_timer" do
+        @em.run do
+          count = 0
+          @em.add_periodic_timer(1) do
+            count += 1
+            if count > 5
+              @em.stop
+            end
+          end
+        end
+      end
+
+      it "should support cancelling periodic_timer" do
+        @em.run do
+          count = 0
+          timer = nil
+          timer = @em.add_periodic_timer(1) do
+            count += 1
+            if count >= 3
+              @em.cancel_timer(timer)
+            end
+          end
+
+          @em.add_timer(5) do
+            @em.stop
+            count.should == 3
+          end
+        end
+      end
+
     end
   end
 
